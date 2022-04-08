f78f858e2948778b6d8db92d37810b7326eb9c98
yakov <yakovrussian@gmail.com>
2022-04-08 20:49:58 +0300

    Added button to unsubscribe with counter. Now need to unsubscribe on click

--- app/src/main/java/com/example/vkunsubscribe/MainActivity.kt
+++ app/src/main/java/com/example/vkunsubscribe/MainActivity.kt
@@ -1,42 +1,32 @@
 package com.example.vkunsubscribe
 
 import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.GridLayout
+import android.widget.Button
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.vk.api.sdk.VK
 import com.vk.api.sdk.VKApiCallback
-import com.vk.api.sdk.VKApiManager
-import com.vk.api.sdk.VKMethodCall
 import com.vk.api.sdk.auth.VKAccessToken
 import com.vk.api.sdk.auth.VKAuthCallback
 import com.vk.api.sdk.auth.VKScope
-import com.vk.api.sdk.exceptions.VKApiExecutionException
 import com.vk.api.sdk.exceptions.VKAuthException
-import com.vk.api.sdk.internal.ApiCommand
-import com.vk.dto.common.id.UserId
-import com.vk.sdk.api.friends.FriendsService
 import com.vk.sdk.api.groups.GroupsService
 import com.vk.sdk.api.groups.dto.GroupsGetObjectExtendedResponse
-import com.vk.sdk.api.groups.dto.GroupsGetResponse
 import com.vk.sdk.api.groups.dto.GroupsGroupFull
 
 class MainActivity : AppCompatActivity(), GroupsAdapter.OnGroupSelectedListener {
 
     private val adapter by lazy { GroupsAdapter(listener = this) }
     private val viewModel: MainViewModel by viewModels()
-    lateinit var userId : UserId
-    lateinit var groupsGetResponse: GroupsGetObjectExtendedResponse
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         VK.login(this, arrayListOf(VKScope.WALL, VKScope.PHOTOS))
+        val mainButton : Button = findViewById(R.id.button)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.also {
@@ -47,15 +37,19 @@ class MainActivity : AppCompatActivity(), GroupsAdapter.OnGroupSelectedListener 
         viewModel.selectedGroups.observe(this, {
             viewModel.nowSelecting.value = it.size > 0
             adapter.updateSelectedGroups(it)
+            mainButton.text = "Отписаться: ${it.size}"
         })
+
+        mainButton.setOnClickListener {
+            viewModel.unsubscribeFromSelected()
+        }
 
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         val callback = object : VKAuthCallback {
             override fun onLogin(token: VKAccessToken) {
-                userId = token.userId
-
+                viewModel.userId.value = token.userId
                 getGroups()
             }
 
@@ -70,10 +64,10 @@ class MainActivity : AppCompatActivity(), GroupsAdapter.OnGroupSelectedListener 
 
     fun getGroups() {
         VK.execute(
-            GroupsService().groupsGetExtended(userId = userId),
+            GroupsService().groupsGetExtended(userId = viewModel.userId.value),
             object : VKApiCallback<GroupsGetObjectExtendedResponse> {
                 override fun success(result: GroupsGetObjectExtendedResponse) {
-                    groupsGetResponse = result
+                    viewModel.groupsGetResponse.value = result
                     /*for (i in result.items) {
 
                     }*/
--- app/src/main/java/com/example/vkunsubscribe/MainViewModel.kt
+++ app/src/main/java/com/example/vkunsubscribe/MainViewModel.kt
@@ -2,18 +2,31 @@ package com.example.vkunsubscribe
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.vk.dto.common.id.UserId
+import com.vk.sdk.api.groups.dto.GroupsGetObjectExtendedResponse
 import com.vk.sdk.api.groups.dto.GroupsGroupFull
 
 class MainViewModel : ViewModel() {
 
-    var selectedGroups: MutableLiveData<MutableList<GroupsGroupFull>> = MutableLiveData(mutableListOf())
+    val selectedGroups: MutableLiveData<MutableList<GroupsGroupFull>> by lazy {
+        MutableLiveData(mutableListOf())
+    }
     var nowSelecting: MutableLiveData<Boolean> = MutableLiveData(false)
+    var userId: MutableLiveData<UserId> = MutableLiveData()
+
+    var groupsGetResponse: MutableLiveData<GroupsGetObjectExtendedResponse> = MutableLiveData()
 
     fun selectGroup(group: GroupsGroupFull) {
         if (selectedGroups.value?.contains(group) == true) {
             selectedGroups.value!!.remove(group)
+            selectedGroups.value = selectedGroups.value
         } else {
             selectedGroups.value?.add(group)
+            selectedGroups.value = selectedGroups.value
         }
+    }
+
+    fun unsubscribeFromSelected() {
+
     }
 }
